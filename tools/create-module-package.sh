80eda2713b72ec3eb8313b8a83006ccd03d2abbe
Ernest Deak <gordon.zar@gmail.com>
2021-10-24 19:19:41 +0200

    more travis stuff

--- tools/create-module-package.sh
+++ tools/create-module-package.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-mkdir ../dist/
-rm ../dist/perl-Program.tgz
+mkdir dist/
 echo "Creating package..."
-tar -czf ../dist/perl-Program.tar.gz ../* --exclude="../dist/" --exclude="../tools/"
+tar -czf dist/perl-Program.tar.gz ./* --exclude="dist/" --exclude="tools/"
 echo "Done"
